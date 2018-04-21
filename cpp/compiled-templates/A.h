e3797d475dabdb4eaa580b3f7fc5c9aeb3aa5272
Michael Bentley <mikebentley15@gmail.com>
2018-04-20 22:12:03 -0600

    cpp/compiled-templates: Play some more and add comments

--- cpp/compiled-templates/A.h
+++ cpp/compiled-templates/A.h
@@ -1,10 +1,15 @@
+#ifndef A_H
+#define A_H
+
 template <typename T>
 class A {
 public:
   A();
-  T val() const;
+  T val() const { return m_val; }
   void val(const T& newval);
   void val(T&& newval);
 private:
   T m_val;
 };
+
+#endif // A_H
--- cpp/compiled-templates/main.cpp
+++ cpp/compiled-templates/main.cpp
@@ -1,20 +1,34 @@
 #include "A.h"
+#include "B.h"
 #include "func.h"
 
 #include <iostream>
 
 int main() {
-  A<int> intwrap;
-  std::cout << "intwrap before assignment: " << intwrap.val() << std::endl;
-  intwrap.val(3);
-  std::cout << "intwrap after assignment:  " << intwrap.val() << std::endl;
+  A<int> A_intwrap;
+  std::cout << "A_intwrap before assignment: " << A_intwrap.val() << std::endl;
+  A_intwrap.val(3);
+  std::cout << "A_intwrap after assignment:  " << A_intwrap.val() << std::endl;
   std::cout << std::endl;
 
-  A<float> flwrap;
-  std::cout << "flwrap before assignment:  " << flwrap.val() << std::endl;
-  flwrap.val(3.1f);
-  std::cout << "flwrap after assignment:   " << flwrap.val() << std::endl;
+  A<float> A_flwrap;
+  std::cout << "A_flwrap before assignment:  " << A_flwrap.val() << std::endl;
+  A_flwrap.val(3.1f);
+  std::cout << "A_flwrap after assignment:   " << A_flwrap.val() << std::endl;
   std::cout << std::endl;
+
+  // This does not work.
+  //B<int> B_intwrap;
+  //std::cout << "A_intwrap before assignment: " << B_intwrap.val() << std::endl;
+  //B_intwrap.val(3);
+  //std::cout << "A_intwrap after assignment:  " << B_intwrap.val() << std::endl;
+  //std::cout << std::endl;
+
+  //B<float> B_flwrap;
+  //std::cout << "A_flwrap before assignment:  " << B_flwrap.val() << std::endl;
+  //B_flwrap.val(3.1f);
+  //std::cout << "A_flwrap after assignment:   " << B_flwrap.val() << std::endl;
+  //std::cout << std::endl;
 
   std::cout << "plus_one(2)   = " << plus_one(2)    << std::endl;
   std::cout << "plus_one(2.1) = " << plus_one(2.1f) << std::endl;
