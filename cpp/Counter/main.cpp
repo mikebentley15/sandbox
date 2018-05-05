e26b86899727529c9f1d7e8d1bddfc009876949c
Michael Bentley <mikebentley15@gmail.com>
2018-05-04 18:55:42 -0600

    Counter: describe work for operator<<

--- cpp/Counter/main.cpp
+++ cpp/Counter/main.cpp
@@ -3,31 +3,16 @@
 #include <iostream>
 #include <string>
 
-template<typename T, template<class,class...> class Cont, class... Args>
-std::ostream& operator<<(std::ostream& out, const Cont<T, Args...> &c) {
-  bool first = true;
-  out << "[";
-  for (const auto &x : c) {
-    if (!first) out << ",";
-    first = false;
-    out << x;
-  }
-  out << "]";
-  return out;
-}
-
-template <typename A, typename B>
-std::ostream& operator<<(std::ostream& out, const std::pair<A,B> &p) {
-  return out << "(" << p.first << "," << p.second << ")";
-}
-
 int main() {
   std::string a("hello there my helper friend");
   Counter<char> c1(a.begin(), a.end());
   UnorderedCounter<char> c2(a.begin(), a.end());
 
+  std::cout << "expression: " << a << std::endl;
   std::cout << "c1: " << c1 << std::endl;
   std::cout << "c2: " << c2 << std::endl;
+  std::cout << "c1['l']: " << c1['l'] << std::endl;
+  std::cout << std::endl;
 
   std::cout << "c3: " << Counter<int>{1,2,3,1,3,1,3,2,3,3,3,2,3,1} << std::endl;
   std::cout << "c4: " << UnorderedCounter<int>{1,2,3,1,3,1,3,2,3,3,3,2,3,1} << std::endl;
