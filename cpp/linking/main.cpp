06be1f9b536c96d0c108f018755038169d8d8393
Michael Bentley <mikebentley15@gmail.com>
2018-06-25 16:12:34 -0600

    linking: some more plaing

--- cpp/linking/main.cpp
+++ cpp/linking/main.cpp
@@ -1,10 +1,18 @@
 #include "foo2.h"
 
 int main() {
-  return unnamed_namespace_inline_hfunc() +
-         unnamed_namespace_hfunc() +
-         static_inline_hfunc() +
-         static_hfunc() +
-         inline_hfunc() +
-         func();
+  // Want this to be
+  //   0 = sum([8,9,10,11,12,3,4,5,6,7]) - sum([13,14,15,16,17])
+  // But can end up being
+  //   5 = sum([8,9,10,11,17,3,4,5,6,7]) - sum([13,14,15,16,17])
+  // Or it can be
+  //   5 = sum([8,9,10,11,12,3,4,5,6,7]) - sum([13,14,15,16,12])
+  return func() -
+         (
+           unnamed_namespace_inline_hfunc() +
+           unnamed_namespace_hfunc() +
+           static_inline_hfunc() +
+           static_hfunc() +
+           inline_hfunc()
+         );
 }
