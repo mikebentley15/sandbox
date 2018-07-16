de53b4463ec7867ec968a97b79477cede45c602e
Michael Bentley <mikebentley15@gmail.com>
2018-07-16 15:47:31 -0600

    range-checker: update

--- python/range-checker/range-checker.py
+++ python/range-checker/range-checker.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # Simple Daikon-style invariant checker
 # Andreas Zeller, May 2012
-# Complete the provided code around lines 28 and 44
-# Do not modify the __repr__ functions.
+# Complete the provided code, using your code from
+# first exercise and adding ability to infer assertions
+# for variable type, set and relations
 # Modify only the classes Range and Invariants,
 # if you need additional functions, make sure
 # they are inside the classes.
@@ -20,33 +21,47 @@ def square_root(x, eps = 0.00001):
 def square(x):
     return x * x
 
+def double(x):
+    return abs(20 * x) + 10
+
 # The Range class tracks the types and value ranges for a single variable.
 class Range:
     def __init__(self):
         self.min  = None  # Minimum value seen
         self.max  = None  # Maximum value seen
+        self.type = None  # Type of variable
+        self.set = set()  # Set of values taken
     
     # Invoke this for every value
     def track(self, value):
-        assert (self.min is None and self.max is None) or \
-               (self.min is not None and self.max is not None)
-        if self.min is None and self.max is None:
+        assert self.type is None or \
+               isinstance(value, type(self.type))
+        assert self.min is None or \
+               self.max is not None or \
+               self.type is not None
+        assert self.max is None or \
+               self.min is not None or \
+               self.type is None
+
+        if self.min is None:
             self.min = value
             self.max = value
         elif self.min > value:
             self.min = value
         elif self.max < value:
             self.max = value
-        else:
-            pass
-            
+        self.type = self.min
+        self.set.add(value)
+        
         assert self.min is not None
         assert self.max is not None
-        assert self.min <= self.max
-            
-    def __repr__(self):
-        return repr(self.min) + ".." + repr(self.max)
+        assert self.type is not None
 
+    def __repr__(self):
+        return repr(self.type) + " " + \
+               repr(self.min) + ".." + \
+               repr(self.max)+ " " + \
+               repr(self.set)
 
 # The Invariants class tracks all Ranges for all variables seen.
 class Invariants:
@@ -58,8 +73,6 @@ class Invariants:
         
     def track(self, frame, event, arg):
         if event == "call" or event == "return":
-            # YOUR CODE HERE. 
-            # MAKE SURE TO TRACK ALL VARIABLES AND THEIR VALUES
             # If the event is "return", the return value
             # is kept in the 'arg' argument to this function.
             # Use it to keep track of variable "ret" (return)
@@ -88,16 +101,19 @@ class Invariants:
         for function, events in self.vars.iteritems():
             for event, vars in events.iteritems():
                 s += event + " " + function + ":\n"
-                # continue
-                
+        
                 for var, range in vars.iteritems():
+                    s += "    assert isinstance({0}, type({1}))\n".format(
+                            var, range.type)
                     s += "    assert "
                     if range.min == range.max:
                         s += var + " == " + repr(range.min)
                     else:
                         s += repr(range.min) + " <= " + var + " <= " + repr(range.max)
                     s += "\n"
-                
+                    # ADD HERE RELATIONS BETWEEN VARIABLES
+                    # RELATIONS SHOULD BE ONE OF: ==, <=, >=
+                    #s += "    assert " + var + " >= " + var2 + "\n"               
         return s
 
 invariants = Invariants()
@@ -109,10 +125,19 @@ def traceit(frame, event, arg):
 sys.settrace(traceit)
 # Tester. Increase the range for more precise results when running locally
 eps = 0.000001
-for i in [3, 0, -10]:
-    z = square_root(i, eps)
-    z = square(z)
+test_vars = [34.6363, 9.348, -293438.402]
+for i in test_vars:
+#for i in range(1, 10):
+    z = double(i)
 sys.settrace(None)
 print invariants
 
+# Example sample of a correct output:
+"""
+return double:
+    assert isinstance(x, type(-293438.402))
+    assert x in set([9.348, -293438.402, 34.6363])
+    assert -293438.402 <= x <= 34.6363
+    assert x <= ret
+"""
 
