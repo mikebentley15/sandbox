341c3b4bdb93415a8b20b2e4111985106220e9e9
Michael Bentley <mikebentley15@gmail.com>
2018-12-08 13:25:51 -0700

    cpp-hier: add failing test for templates within template params

---
+++ cpp/parser-hierarchy/tests/testout-5.cpp
@@ -0,0 +1,6 @@
+template < typename Dst, template < typename > class StorageBase, typename Src, typename Func > void call_assignment(NoAlias < Dst, StorageBase >& dst, const Src & src, const Func & func) {
+ call_assignment_no_alias(dst . expression(), src, func);
+}
+template < typename Dst, typename Src, typename Func > void call_assignment_no_alias(Dst & dst, const Src & src, const Func & func) {
+ enum { NeedToTranspose = ((int(Dst :: RowsAtCompileTime) == 1 && int(Src :: ColsAtCompileTime) == 1) || (int(Dst :: ColsAtCompileTime) == 1 && int(Src :: RowsAtCompileTime) == 1)) && int(Dst :: SizeAtCompileTime) != 1 };
+}
