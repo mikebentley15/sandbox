c14f61e4c7de986135406df241458bb8fb2e492b
Michael Bentley <mikebentley15@gmail.com>
2019-05-16 16:21:55 -0600

    mpi: add no-mpi-calls.c

---
+++ c/mpi/no-mpi-calls.c
@@ -0,0 +1,13 @@
+#include <mpi.h>
+
+#include <stdio.h>
+
+int main(int argCount, char *argList[]) {
+  printf("hello\n");
+  //MPI_Init(&argCount, &argList);
+  //MPI_Finalize();
+  //int rank;
+  //MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  //printf("My rank: %d\n", rank);
+  return 0;
+}
