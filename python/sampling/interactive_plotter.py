c6ae1868bcccc126d480cb0bd34f1c97fb7146ad
Michael Bentley <mikebentley15@gmail.com>
2019-06-27 22:35:04 -0600

    python/sampling: add interval to function params

--- python/sampling/interactive_plotter.py
+++ python/sampling/interactive_plotter.py
@@ -3,13 +3,13 @@ import matplotlib.animation as animation
 import time
 import numpy as np
 
-def animate_plot_2d(generator):
+def animate_plot_2d(generator, interval=200):
     '''
     Plots between 0 and 1 for both dimensions.  Will plot one additional point
     from the generator every time_delta seconds.  
 
     @param generator: generates the next point to plot (numpy.array)
-    @param time_delta: seconds between replotting with the new point
+    @param interval: milliseconds between replotting with the new point
     '''
     fig, ax = plt.subplots()
     scatter = ax.plot([], [], 'bo', ms=7)[0]
@@ -19,5 +19,6 @@ def animate_plot_2d(generator):
         data = scatter.get_data()
         scatter.set_data(np.concatenate((data[0], [point[0]])),
                          np.concatenate((data[1], [point[1]])))
-    ani = animation.FuncAnimation(fig, plot_points, generator)
+    ani = animation.FuncAnimation(fig, plot_points, generator,
+                                  interval=interval)
     plt.show()
