a2cc8a557ed14dfaa399464ebddf268466df4d2d
Michael Bentley <mikebentley15@gmail.com>
2019-06-27 22:25:41 -0600

    cpp/vector/operator_equal.cpp: get working

--- cpp/vector/operator_equal.cpp
+++ cpp/vector/operator_equal.cpp
@@ -3,15 +3,17 @@
 
 namespace pt {
   struct Point { int a, b; };
+
+  bool operator==(const Point lhs, const Point rhs) {
+    return lhs.a == rhs.a && lhs.b == rhs.b;
+  }
 }
 
-bool operator==(const pt::Point& lhs, const pt::Point& rhs) {
-  return lhs.a == rhs.a && lhs.b == rhs.b;
-}
+using pt::operator==;
 
 int main() {
-  std::vector<pt::Point> x { {1, 2}, {1, 3}, {4, 2}};
-  std::vector<pt::Point> y { {1, 2}, {1, 3}, {4, 2}};
+  std::vector<pt::Point> x { {1, 2}, {1, 3}, {4, 2} };
+  std::vector<pt::Point> y { {1, 2}, {1, 3}, {4, 2} };
 
   std::cout << "x == y ? " << (x == y ? "true" : "false") << std::endl;
 
