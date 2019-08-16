d32e9cb0a03af1e95850bab57f4bd1625a6f891d
Michael Bentley <mikebentley15@gmail.com>
2019-08-16 08:45:43 -0600

    python/sockets/echo_client: add timeout

--- python/sockets/echo/echo_client.py
+++ python/sockets/echo/echo_client.py
@@ -3,7 +3,7 @@
 'Simple client that sends a small message via TCP'
 
 import argparse
-from socket import socket, AF_INET, SOCK_STREAM
+from socket import socket, AF_INET, SOCK_STREAM, timeout as Timeout
 import sys
 
 # AF_INET: means IPv4
@@ -16,14 +16,23 @@ def parse_args(arguments):
         )
     parser.add_argument('-p', '--port', type=int, default=65432,
                         help='Port to connect to.')
+    parser.add_argument('-t', '--timeout', type=float, default=1.5,
+                        help='''
+                            Timeout.  Set to a negative number to disable
+                            timeout.  Set to 0 to have an instant timeout.
+                            ''')
     parser.add_argument('host', default='127.0.0.1', nargs='?',
                         help='ip or hostname of destination')
     parser.add_argument('message', default='Hello world!', nargs='?',
                         help='what to send')
-    return parser.parse_args(arguments)
+    args = parser.parse_args(arguments)
+    if args.timeout < 0:
+        args.timeout = None
+    return args
 
-def send(host, port, message):
+def send(host, port, message, timeout=1.5):
     with socket(AF_INET, SOCK_STREAM) as s:
+        s.settimeout(timeout)
         s.connect((host, port))
         print(f'sending "{message}" to {host}:{port}')
         s.sendall(message.encode('utf-8'))
@@ -33,10 +42,12 @@ def send(host, port, message):
 def main(arguments):
     args = parse_args(arguments)
     try:
-        send(args.host, args.port, args.message)
+        send(args.host, args.port, args.message, timeout=args.timeout)
     except ConnectionRefusedError:
         print('Connection refused', file=sys.stderr)
         return 1
+    except Timeout:
+        print('Connection timed out', file=sys.stderr)
     return 0
 
 if __name__ == '__main__':
