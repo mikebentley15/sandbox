aba26a95183496f153cd7d36e4906f759dd6715a
Michael Bentley <mikebentley15@gmail.com>
2020-04-22 23:03:41 -0600

    ex01: populate with minimal example

--- cpp/casadi/apps/ex01.cpp
+++ cpp/casadi/apps/ex01.cpp
@@ -1,5 +1,16 @@
 #include <casadi/casadi.hpp>
 
+#include <vector>
+
 int main() {
+  using namespace casadi;
+
+  auto x = MX::sym("x", 5, 1);
+  auto y = norm_2(x);
+  auto grad_y = gradient(y, x);
+  auto f = Function("f", {x}, {grad_y});
+  auto grad_y_num = f(DM({1,2,3,4,5}));
+  std::cout << grad_y_num << std::endl;
+
   return 0;
 }
