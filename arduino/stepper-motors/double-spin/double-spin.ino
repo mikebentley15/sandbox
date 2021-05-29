c6231d1ed138eeaa268b96101d2dad6084b3bb58
Michael Bentley <mikebentley15@gmail.com>
2021-05-28 21:52:49 -0600

    arduino/stepper/doublespin: enable both motors

--- arduino/stepper-motors/double-spin/double-spin.ino
+++ arduino/stepper-motors/double-spin/double-spin.ino
@@ -6,40 +6,42 @@
 // defines pins numbers
 const int linear_step_pin = 5;
 const int linear_dir_pin  = 6;
-const int rotary_step_pin = 5;
-const int rotary_dir_pin  = 6;
+const int rotary_step_pin = 2;
+const int rotary_dir_pin  = 3;
 
 void setup() {
   // Sets the two pins as Outputs
   pinMode(linear_step_pin, OUTPUT);
   pinMode(linear_dir_pin,  OUTPUT);
-  //pinMode(rotary_step_pin, OUTPUT);
-  //pinMode(rotary_dir_pin,  OUTPUT);
+  pinMode(rotary_step_pin, OUTPUT);
+  pinMode(rotary_dir_pin,  OUTPUT);
 }
 void loop() {
   digitalWrite(linear_dir_pin, HIGH); // Enables the motor to move in a particular direction
-  //digitalWrite(rotary_dir_pin, HIGH); // Enables the motor to move in a particular direction
+  digitalWrite(rotary_dir_pin, HIGH); // Enables the motor to move in a particular direction
+
+  int microwait = 1000;
 
   // Makes 200 pulses for making one full cycle rotation
   for(int x = 0; x < 400; x++) {
     digitalWrite(linear_step_pin, HIGH);
-    //digitalWrite(rotary_step_pin, HIGH);
-    delayMicroseconds(2000);
+    digitalWrite(rotary_step_pin, HIGH);
+    delayMicroseconds(microwait);
     digitalWrite(linear_step_pin, LOW);
-    //digitalWrite(rotary_step_pin, LOW);
-    delayMicroseconds(2000);
+    digitalWrite(rotary_step_pin, LOW);
+    delayMicroseconds(microwait);
   }
 
   // Makes 400 pulses for making two full cycle rotation
   digitalWrite(linear_dir_pin, LOW); // Enables the motor to move in a particular direction
-  //digitalWrite(rotary_dir_pin, LOW); // Enables the motor to move in a particular direction
+  digitalWrite(rotary_dir_pin, LOW); // Enables the motor to move in a particular direction
   for(int x = 0; x < 400; x++) {
     digitalWrite(linear_step_pin, HIGH);
-    //digitalWrite(rotary_step_pin, HIGH);
-    delayMicroseconds(2000);
+    digitalWrite(rotary_step_pin, HIGH);
+    delayMicroseconds(microwait);
     digitalWrite(linear_step_pin, LOW);
-    //digitalWrite(rotary_step_pin, LOW);
-    delayMicroseconds(2000);
+    digitalWrite(rotary_step_pin, LOW);
+    delayMicroseconds(microwait);
   }
   delay(1000);
 }
