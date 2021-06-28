c9c830e6057400f03bc372f7bccc06f69613e98b
Michael Bentley <mikebentley15@gmail.com>
2021-06-27 21:13:17 -0600

    python/ai/minimax: add more to tictactoe.py

--- python/ai/minimax/tictactoe.py
+++ python/ai/minimax/tictactoe.py
@@ -15,8 +15,21 @@ class Player(IntEnum):
     P2 = 2
 
 class TicTacToe:
+    '''
+    Represents an instance of a tic-tac-toe game.
+
+    There is a start_state which is the initial state of the game.
+
+    A game state is handled internally, but for those who care, it is
+    represented as a tuple of 11 values, each of which is a Player value:
+
+    - state[0]: who's turn it is
+    - state[1-9]: the board laid out in row-major order starting from the top
+    - state[10]: the winner
+    '''
+
     def __init__(self):
-        self.start_state = tuple([Player.EMPTY] * 11)
+        self.start_state = (Player.P1,) + (Player.EMPTY,) * 10
 
     def equivalent_states(self, state):
         '''
@@ -77,3 +90,68 @@ class TicTacToe:
         the equivalent states that is considered the official one.
         '''
         return max(self.equivalent_states(state))
+
+    def available_actions(self, state):
+        'Returns available actions by the current player in the current state'
+        if state[0] == Player.EMPTY:  # game is over
+            return []
+        return [i for i in range(1, 10) if state[i] == Player.EMPTY]
+
+    def move(self, state, action):
+        '''
+        Give the resultant state after the current player does the given
+        action.
+        '''
+        assert state[action] == Player.EMPTY
+        assert state[0] != Player.EMPTY, 'cannot move if the game is over'
+        assert state[10] == Player.EMPTY, 'cannot move if the game is over'
+
+        p = state[0] # current player
+
+        # check to see if we have a winner
+        newstate = list(state)
+        if self._is_winning_move(state, action):
+            newstate[0] = Player.EMPTY
+            newstate[10] = p
+        else:
+            newstate[0] = Player.P2 if state[0] == Player.P1 else Player.P1
+
+        # fill in that square
+        newstate[action] = p
+
+        # if the board is full, mark it as a tie
+        if not any(x == Player.EMPTY for x in newstate[1:10]):
+            newstate[0] = Player.EMPTY
+            newstate[10] = Player.EMPTY
+
+        return tuple(newstate)
+
+    def _is_winning_move(self, state, action):
+        '''
+        Returns true if the action is a winning move for the current player
+
+        @param state: a state tuple
+        @param action: index into the state tuple for the current player
+        '''
+        assert state[action] == Player.EMPTY
+        assert state[0] != Player.EMPTY, 'cannot move if the game is over'
+        assert state[10] == Player.EMPTY, 'cannot move if the game is over'
+
+        p = state[0] # current player
+        s = state
+
+        to_check = {
+            1 : ((2, 3), (4, 7), (5, 9)),
+            2 : ((1, 3), (5, 8)),
+            3 : ((1, 2), (6, 9), (5, 7)),
+            4 : ((5, 6), (1, 7)),
+            5 : ((1, 9), (2, 8), (3, 7), (4, 6)),
+            6 : ((3, 9), (4, 5)),
+            7 : ((1, 4), (8, 9), (5, 3)),
+            8 : ((7, 9), (2, 5)),
+            9 : ((7, 8), (6, 3), (1, 5)),
+            }
+
+        return any(p == s[x[0]] and p == s[x[1]]
+                   for x in to_check[action])
+
