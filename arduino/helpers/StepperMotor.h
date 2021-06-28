71d7a3554d905704ff6d954c2780bd6cc8f6ac91
Michael Bentley <mikebentley15@gmail.com>
2021-06-28 16:10:59 -0600

    arduino/helpers/StepperMotor.h: add micros_per_pulse() and next_pulse() static methods
    - micros_per_pulse(): calculates pulse interval for a specific angular velocity
    - next_pulse(): calculates the next pulse interval and new velocity at that
      next pulse to achieve a desired acceleration starting at a given velocity.

--- arduino/helpers/StepperMotor.h
+++ arduino/helpers/StepperMotor.h
@@ -1,7 +1,13 @@
 #ifndef STEPPER_MOTOR_H
 #define STEPPER_MOTOR_H
 
+#include "mockstd.h"
+
+#include <math.h>
+
 struct StepperMotor {
+  const uint32_t millidegrees_per_step = 1800;  // 200 steps per revolution
+
   int step_pin;        // to send pulses for stepping
   int dir_pin;         // to set the direction (high is forward)
   bool forward;        // if we are set to moving forward
@@ -53,6 +59,40 @@ struct StepperMotor {
   void go_backward() {
     digitalWrite(dir_pin, LOW);
     forward = false;
+  }
+
+  /** calculate the microseconds per pulse for a velocity
+   *
+   * @param vel: angular velocity in milli-degrees per second
+   * @return closest integer microsecond interval for each pulse to achieve the
+   *     given velocity.
+   */
+  static constexpr uint32_t micros_per_pulse(uint32_t vel) {
+    // 1.8e9 = (1e6 us/s) * (1e3 m-deg/deg) * (360 deg/rev) / (200 pulses/rev)
+    return uint32_t(1800000000) / vel;
+  }
+
+  /** calculate the next pulse interval for an acceleration
+   *
+   * assumes it is called immediately after calling a pulse with interval
+   * micros_per_pulse(vel).
+   *
+   * performs a square root using double precision, so it's not the most
+   * efficient thing...
+   *
+   * @param vel: current velocity in millidegrees per second
+   * @param acc: desired acceleration in millidegrees per second squared
+   * @return pulse interval: microseconds until the next pulse (assuming this
+   *     is called right after a pulse is sent)
+   * @return new velocity: the new velocity at the next pulse in millidegrees
+   *     per second.
+   */
+  static pair<uint32_t, uint32_t> next_pulse(uint32_t vel, uint32_t acc) {
+    double dvel(vel);
+    double dacc(acc);
+    double t = (- dvel + sqrt(dvel * dvel + 4 * dacc * 1.8e9)) / (2 * dacc);
+    uint32_t new_vel = uint32_t(dacc * t);
+    return pair<uint32_t, uint32_t>(uint32_t(t), new_vel);
   }
 };
 
