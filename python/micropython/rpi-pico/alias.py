8f01b80a02a336ff8c4f13019a6fbec673b19b8a
Michael Bentley <mikebentley15@gmail.com>
2021-09-14 16:42:13 -0600

    python/pico: add some stuff to alias.py

--- python/micropython/rpi-pico/alias.py
+++ python/micropython/rpi-pico/alias.py
@@ -1,23 +1,37 @@
-import os
+import os as _os
+import hashlib as _hl
+import binascii as _ba
+import micropython as _mp
+import gc as _gc
+import sys as _sys
 
 print('importing all aliases')
 
-cd = os.chdir
-ils = os.ilistdir
-ls = os.listdir
-mv = os.rename
-pwd = os.getcwd
-rm = os.remove
-rmdir = os.rmdir
-uname = os.uname
-mkdir = os.mkdir
 
-def mkdir_p(path):
-    try:
-        mkdir(path)
-    except OSError as ex:
-        if ex.errno != 17:
-            raise ex
+# =========================
+# direct and simple aliases
+# =========================
+
+# filesystem and system manipulation
+cd    = _os.chdir
+ils   = _os.ilistdir
+ls    = _os.listdir
+mkdir = _os.mkdir
+mv    = _os.rename
+pwd   = _os.getcwd
+rm    = _os.remove
+rmdir = _os.rmdir
+uname = _os.uname
+
+# micropython stuff
+const = _mp.const
+meminfo = _mp.mem_info
+free  = _gc.collect
+
+
+# ======================
+# more complex functions
+# ======================
 
 def read(fname):
     with open(fname, 'r') as f:
@@ -25,6 +39,23 @@ def read(fname):
 
 def cat(fname):
     print(read(fname))
+
+def sha256(fname=None, content=None):
+    if fname and contents:
+        print('sha256: cannot specify both filename and contents', file=_sys.stderr)
+    if fname:
+        contents = read(fname)
+    if contents:
+        h = _hl.sha256(contents)
+        return _ba.hexlify(h.digest())
+    return None
+
+def mkdir_p(path):
+    try:
+        mkdir(path)
+    except OSError as ex:
+        if ex.errno != 17:
+            raise ex
 
 def pp(item):
     'pretty-print'
@@ -54,3 +85,23 @@ def pp(item):
             print(' }')
     else:
         print(repr(item))
+
+def ppdir(item, hidden=False, sort=True):
+    contents = dir(item)
+    if not hidden:
+        contents = [x for x in dir(item) if not x.startswith('_')]
+    if sort:
+        contents.sort()
+    pp(contents)
+
+# TODO: implement file grep
+# TODO: implement simple file sed (i.e., search and replace of file contents)
+# TODO: implement column with csv data
+# TODO: add sort and unique flags for cat()
+# TODO: very simple docstrings
+
+
+# list of provided aliases
+def aliases():
+    return sorted(x for x in dir(_sys.modules[__name__])
+                  if not x.startswith('_') and x != 'aliases')
