60adb1763fc34fd63424ed3bc2ba92fe19efe176
Michael Bentley <mikebentley15@gmail.com>
2022-02-23 08:58:46 -0700

    python/blender/paths/waypoints.py: some updates

--- python/blender/paths/waypoints.py
+++ python/blender/paths/waypoints.py
@@ -12,9 +12,6 @@ import csv
 import os
 import sys
 
-sys.path.append(os.path.join(
-    os.environ['HOME'], 'git', 'armlab', 'tendon_experiments', 'build'))
-
 workdir = os.path.dirname(os.path.abspath(__file__))
 #sys.path.append(workdir)
 
@@ -22,36 +19,46 @@ def populate_parser(parser=None):
     if parser is None:
         parser = argparse.ArgumentParser()
     parser.description = 'Creates a blend file with the given waypoints as a path'
-    parser.add_argument('points_csv')
+    parser.add_argument('problem_toml')
+    parser.add_argument('configs_csv')
     return parser
 
 def main(arguments):
+    print(sys.version)
+    print(sys.executable)
+
+    sys.path.append(os.path.join(
+        os.environ['HOME'], 'git', 'armlab', 'tendon_experiments', 'build-blender'))
+
+    import cpptendon as tend
+
     parser = populate_parser()
     args = parser.parse_args(arguments)
 
-    with open(args.points_csv, 'r') as fin:
-        reader = csv.DictReader(fin)
-        pts = [Vector((float(row['x']), float(row['y']), float(row['z'])))
-               for row in reader]
+    prob = tend.motion_planning.Problem.from_toml(args.problem_toml)
+    qs = prob.load_plan(args.configs_csv)
+    shapes = [prob.robot.forward_kinematics(q) for q in qs]
 
-    ops.curve.primitive_bezier_curve_add(enter_editmode=True)
-    ops.curve.subdivide(number_cuts=len(pts)-2)
+    for i, shape in enumerate(shapes):
+        ops.curve.primitive_bezier_curve_add(enter_editmode=True)
+        ops.curve.subdivide(number_cuts=len(shape)-2)
 
-    curve = bpy.context.active_object
-    spline = curve.data.splines[-1]
-    bezier_points = spline.bezier_points
-    for knot, pt in zip(bezier_points, pts):
-        knot.co = pt
-        # handle types:
-        # - 'AUTO': smooth
-        # - 'VECTOR': piecewise linear
-        # - 'FREE': completely free
-        knot.handle_left_type  = 'AUTO'
-        knot.handle_right_type = 'AUTO'
-        #knot.handle_left_type  = 'VECTOR'
-        #knot.handle_right_type = 'VECTOR'
+        curve = bpy.context.active_object
+        curve.name = f'backbone_{i:03d}'
+        spline = curve.data.splines[-1]
+        bezier_points = spline.bezier_points
+        for knot, pt in zip(bezier_points, shape):
+            knot.co = pt
+            # handle types:
+            # - 'AUTO': smooth
+            # - 'VECTOR': piecewise linear
+            # - 'FREE': completely free
+            knot.handle_left_type  = 'AUTO'
+            knot.handle_right_type = 'AUTO'
+            #knot.handle_left_type  = 'VECTOR'
+            #knot.handle_right_type = 'VECTOR'
 
-    ops.object.mode_set(mode='OBJECT')
+        ops.object.mode_set(mode='OBJECT')
 
     print('Creating waypoints.blend')
     bpy.ops.wm.save_as_mainfile(filepath=os.path.join(workdir, 'waypoints.blend'))
