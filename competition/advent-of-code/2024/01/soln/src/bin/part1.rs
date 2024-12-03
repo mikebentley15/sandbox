d96d3b480e7b865b3fde0019ff43cb69976f2894
Michael Bentley <michael.bentley@motional.com>
2024-12-02 21:12:46 -0700

    advent/01/part1: remove debug printing

--- competition/advent-of-code/2024/01/soln/src/bin/part1.rs
+++ competition/advent-of-code/2024/01/soln/src/bin/part1.rs
@@ -3,17 +3,8 @@ use soln::common::*;
 fn main() {
     let fname = get_input_file();
     let (mut a, mut b) = create_vectors_from_input(&fname);
-    let debug = false;
-
     a.sort();
     b.sort();
-    if debug {
-        println!("Sorted lists with distances:");
-        for (aval, bval) in a.iter().zip(b.iter()) {
-            println!("{} {} : {}", aval, bval, (aval - bval).abs());
-        }
-    }
-
     let total_distance = a
         .iter()
         .zip(b.iter())
